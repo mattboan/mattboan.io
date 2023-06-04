d3923d649d418e46995d5b893df4d2daf930af52
mattboan <matt.n.boan@gmail.com>
2023-06-04 10:59:25 +0800

    Try this link thing

--- src/comps/ProjectCard.tsx
+++ src/comps/ProjectCard.tsx
@@ -1,17 +1,14 @@
 import styles from '@/styles/project-card.comp.module.scss';
 import { Project } from '@/db/project.def';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export const ProjectCard = ({ project }: { project: Project }) => {
-    const router = useRouter();
-
     return (
-        <div
+        <Link
             className={styles.project_card}
             tabIndex={0}
-            role="link"
-            onClick={() => router.push(`/projects/${project?.slug}`)}
+            href={`/projects/${project?.slug}`}
         >
             <Image
                 src={project.header_img || '/img/tmp.png'}
@@ -21,6 +18,6 @@ export const ProjectCard = ({ project }: { project: Project }) => {
             />
             <h3>{project.heading}</h3>
             <p>{project.overview}</p>
-        </div>
+        </Link>
     );
 };
